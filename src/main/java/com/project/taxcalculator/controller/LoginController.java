4ee9ce356e399d2a63f7faabca1681d06e786465
ThaminduMadhushan <punyawa-im20022@stu.kln.ac.lk>
2024-02-25 21:54:22 +0530

    login

--- src/main/java/com/project/taxcalculator/controller/LoginController.java
+++ src/main/java/com/project/taxcalculator/controller/LoginController.java
@@ -12,29 +12,50 @@ import java.io.IOException;
 public class LoginController extends HttpServlet {
 
     public void init() {
-
     }
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        super.doGet(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-
-        //Storing the parameters as variables
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Retrieve username and password from the request
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        //Create new Login object to store the parameters
-        User user = new User();
-        login.setUsername(username);
-        login.setPassword(password);
+        // Validate the user credentials using UserDao
+        UserDao userDao = new UserDao(DatabaseConnection.getConnection());
+        User user = userDao.getUserByUsernameAndPassword(username, password);
 
-        //Setting the role of the user
-        if(username.equals("admin")) {
-            login.setRole("admin");
+        if (user != null) {
+            // If the user is valid, set a session attribute indicating successful login
+            request.getSession().setAttribute("loggedInUser", user);
+            // Redirect to some success page
+            response.sendRedirect("success.jsp");
         } else {
-            login.setRole("user");
+            // If the user is not valid, redirect back to the login page with an error message
+            response.sendRedirect("login.jsp?error=invalid");
         }
+    }
+
 }
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        super.doGet(request, response);
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        response.setContentType("text/html");
+//
+//        //Storing the parameters as variables
+//        String username = request.getParameter("username");
+//        String password = request.getParameter("password");
+//
+//        //Create new Login object to store the parameters
+//        User user = new User();
+//        login.setUsername(username);
+//        login.setPassword(password);
+//
+//        //Setting the role of the user
+//        if(username.equals("admin")) {
+//            login.setRole("admin");
+//        } else {
+//            login.setRole("user");
+//        }
+//}
